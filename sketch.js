857b37b3aa9880a47afa28ca86310e294aa63510
agent2512 <agent2512@gmail.com>
2025-03-17 22:52:16 +0100

    refactor: update block classes and introduce ZombieBlock with new neighbor logic

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
-const s = 10;
+const s = 2;
 let blocks = {};
 let newBlocks = {};
 let test;
 
 function setup() {
   createCanvas(800, 800);
-  frameRate(1);
+  randomSeed(0);
+  frameRate(5);
   for (let i = 0; i < width / s; i++) {
     for (let j = 0; j < height / s; j++) {
-      const newC = random([LifeBlock, DeadBlock, poisonBlock]);
+      const newC = random([LifeBlock, DeadBlock, ZombieBlock]);
       const x = i;
       const y = j;
 
@@ -27,11 +28,31 @@ function draw() {
     newBlocks[`${block.x}-${block.y}`] = newBlock;
   }
 
+  if (JSON.stringify(blocks) === JSON.stringify(newBlocks)) {
+    console.log("done");
+    noLoop();
+  } else {
+    // log the difference
+    const diffKeys = Object.keys(blocks).filter(
+      (key) => blocks[key] !== newBlocks[key]
+    );
+    const diff = diffKeys.map((key) => ({
+      key,
+      old: blocks[key],
+      new: newBlocks[key],
+    }));
+    console.log(diff);
+  }
+
   blocks = newBlocks;
   newBlocks = {};
   console.log(g);
   g++;
 }
+
+////////////////////////////////////////////
+// Base Block Class
+////////////////////////////////////////////
 
 class Block {
   constructor(x, y) {
@@ -49,21 +70,27 @@ class Block {
     square(this.showX, this.showY, s);
   }
 
-  findNeighbors() {
+  // n is the distance to the neighbors
+  findNeighbors(n) {
+    // Returns four direct neighbors plus an array with those that exist.
     return {
-      up: blocks[`${this.x}-${this.y - 1}`],
-      down: blocks[`${this.x}-${this.y + 1}`],
-      left: blocks[`${this.x - 1}-${this.y}`],
-      right: blocks[`${this.x + 1}-${this.y}`],
+      up: blocks[`${this.x}-${this.y - n}`],
+      down: blocks[`${this.x}-${this.y + n}`],
+      left: blocks[`${this.x - n}-${this.y}`],
+      right: blocks[`${this.x + n}-${this.y}`],
       array: [
-        blocks[`${this.x}-${this.y - 1}`],
-        blocks[`${this.x}-${this.y + 1}`],
-        blocks[`${this.x - 1}-${this.y}`],
-        blocks[`${this.x + 1}-${this.y}`],
+        blocks[`${this.x}-${this.y - n}`],
+        blocks[`${this.x}-${this.y + n}`],
+        blocks[`${this.x - n}-${this.y}`],
+        blocks[`${this.x + n}-${this.y}`],
       ].filter(Boolean),
     };
   }
 }
+
+////////////////////////////////////////////
+// Block subClass
+////////////////////////////////////////////
 
 class LifeBlock extends Block {
   constructor(x, y) {
@@ -73,14 +100,11 @@ class LifeBlock extends Block {
   }
 
   update() {
-    const neighbors = this.findNeighbors().array;
+    const neighbors = this.findNeighbors(1).array;
     const deadNeighbors = neighbors.filter((n) => n.type === "dead");
-    if (deadNeighbors.length == neighbors.length) {
+    // If ALL neighbors are dead, transform to dead
+    if (deadNeighbors.length === neighbors.length) {
       return new DeadBlock(this.x, this.y);
-    }
-    const poisonNeighbors = neighbors.filter((n) => n.type === "poison");
-    if (poisonNeighbors.length > neighbors.length / 2) {
-      return new poisonBlock(this.x, this.y);
     }
 
     return this;
@@ -95,9 +119,10 @@ class DeadBlock extends Block {
   }
 
   update() {
-    const neighbors = this.findNeighbors().array;
+    const neighbors = this.findNeighbors(1).array;
     const lifeNeighbors = neighbors.filter((n) => n.type === "life");
-    if (lifeNeighbors.length == neighbors.length) {
+    // If all neighbors are life, become life
+    if (lifeNeighbors.length === neighbors.length) {
       return new LifeBlock(this.x, this.y);
     }
 
@@ -105,24 +130,33 @@ class DeadBlock extends Block {
   }
 }
 
-class poisonBlock extends Block {
+////////////////////////////////////////////////
+// New Block Subclass: ZombieBlock
+////////////////////////////////////////////////
+
+class ZombieBlock extends Block {
   constructor(x, y) {
     super(x, y);
-    this.color = [0, 255, 0];
-    this.type = "poison";
+    // Purplish color
+    this.color = color(128, 0, 128);
+    this.type = "zombie";
   }
 
   update() {
-    const neighbors = this.findNeighbors().array;
+    const neighbors = this.findNeighbors(1).array;
     const lifeNeighbors = neighbors.filter((n) => n.type === "life");
-    const notLifeNeighbors = neighbors.filter((n) => n.type !== "life");
-    if (lifeNeighbors.length > notLifeNeighbors.length) {
+    const deadNeighbors = neighbors.filter((n) => n.type === "dead");
+
+    // Example logic:
+    // If we have 3 or more life neighbors, become life
+    // Else if we have 3 or more dead neighbors, become dead
+    // Otherwise remain a zombie
+
+    if (lifeNeighbors.length >= 3) {
       return new LifeBlock(this.x, this.y);
-    }
-    if (lifeNeighbors.length < notLifeNeighbors.length) {
+    } else if (deadNeighbors.length >= 3) {
       return new DeadBlock(this.x, this.y);
     }
-
     return this;
   }
 }
